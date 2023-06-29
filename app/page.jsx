4dcb33af5f98ae4cb14f3e7c04e03f90372c23ce
devopsmeysam <91230909+devopsmeysam@users.noreply.github.com>
2023-06-29 14:57:15 -0400

    Finalizes Home Page and starts the Create Post

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,17 @@
+import Feed from "@components/Feed";
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <section className="w-full flex-center flex-col">
+      <h1 className="head_text text-center">
+        Discover and Share
+        <br className="max-md:hidden" />
+        <span className="orange_gradient text-center">AI-Powered Prompts</span>
+      </h1>
+      <p className="desc text-center">Promptopia is a cutting-edge AI tool that fosters exploration, innovation, and collaboration in the realm of creative prompts. Designed for the contemporary world, this open-source platform empowers users to uncover, generate, and distribute captivating prompts, fueling inspiration and artistic expression</p>
+
+      <Feed />
+    </section>
   )
 }
 
